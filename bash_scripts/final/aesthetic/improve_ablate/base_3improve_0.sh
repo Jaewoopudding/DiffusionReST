28f267601ecd19e34330bef123ca037b1281ec9c
Jaewoopudding <ijnn258@gmail.com>
2025-08-09 16:23:36 +0900

    script update

--- bash_scripts/final/aesthetic/improve_ablate/base_3improve_0.sh
+++ bash_scripts/final/aesthetic/improve_ablate/base_3improve_0.sh
@@ -4,7 +4,7 @@ accelerate launch --main_process_port 29501 scripts/train_mcts_mle.py \
 --config.reward_fn aesthetic_score_diff \
 --config.prompt_fn simple_animals \
 --config.train.type 'energy_based_negative_gradient' \
---config.train.learning_rate 3e-4 \
+--config.train.learning_rate 1e-3 \
 --config.sample.num_steps 50 \
 --config.num_epochs 100 \
 --config.save_freq 2 \

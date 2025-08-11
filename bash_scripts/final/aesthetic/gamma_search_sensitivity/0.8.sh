0bb42a8cb1bd05fb3d59758ef680be9259fc87eb
Jaewoopudding <ijnn258@gmail.com>
2025-08-11 16:09:20 +0900

    scripts update

---
+++ bash_scripts/final/aesthetic/gamma_search_sensitivity/0.8.sh
@@ -0,0 +1,23 @@
+accelerate launch --main_process_port 29501 scripts/train_mcts_mle.py \
+--config config/svdd_aesthetic_mle.py \
+--config.run_name gamma_0.8 \
+--config.reward_fn aesthetic_score_diff \
+--config.prompt_fn simple_animals \
+--config.train.type 'energy_based_negative_gradient' \
+--config.train.learning_rate 1e-3 \
+--config.sample.num_steps 50 \
+--config.num_epochs 100 \
+--config.save_freq 5 \
+--config.multistep_mdp \
+--config.seed 0 \
+--config.sample.num_prompts_per_batch 8 \
+--config.sample.num_batches_per_epoch 8 \
+--config.search.value_gradient \
+--config.search.kl_lagrangian_coef 0.005 \
+--config.search.duplicate 4 \
+--config.search.gamma 0.8 \
+--config.search.importance_sampling \
+--config.search.hill_climbing \
+--config.train.improve_steps 1 \
+--noconfig.train.negative_gradient \
+--config.train.kl_coef 0.01 \

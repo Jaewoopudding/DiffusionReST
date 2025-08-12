55d323ff16e74962632c0b30efb19e738cd743d1
Jaewoopudding <ijnn258@gmail.com>
2025-08-12 23:54:26 +0900

    update config

--- config/svdd_aesthetic_mle.py
+++ config/svdd_aesthetic_mle.py
@@ -17,7 +17,7 @@ def get_config():
     # number of epochs between saving model checkpoints.
     config.save_freq = 5
     # number of checkpoints to keep before overwriting old ones.
-    config.num_checkpoint_limit = 5
+    config.num_checkpoint_limit = 50
     # mixed precision training. options are "fp16", "bf16", and "no". half-precision speeds up training significantly.
     config.mixed_precision = "fp16"
     # allow tf32 on Ampere GPUs, which can speed up training.

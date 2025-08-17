b5fdabacf2262e514d1107a11d74833a7580436e
Jaewoopudding <ijnn258@gmail.com>
2025-08-17 20:10:33 +0900

    update script

--- bash_scripts/final/aesthetic/M-step_ablation/base_2.sh
+++ bash_scripts/final/aesthetic/M-step_ablation/base_2.sh
@@ -5,8 +5,8 @@ accelerate launch --main_process_port 29501 scripts/train_mcts_mle.py \
 --config.prompt_fn simple_animals \
 --config.train.type 'energy_based_negative_gradient' \
 --config.train.learning_rate 1e-3 \
---config.sample.num_steps 50 \
---config.num_epochs 100 \
+--config.sample.num_steps 30 \
+--config.num_epochs 30 \
 --config.save_freq 5 \
 --config.multistep_mdp \
 --config.seed 2 \
@@ -21,3 +21,4 @@ accelerate launch --main_process_port 29501 scripts/train_mcts_mle.py \
 --config.train.improve_steps 1 \
 --noconfig.train.negative_gradient \
 --config.train.kl_coef 0.01 \
+--config.eval.eval_freq 5 \
